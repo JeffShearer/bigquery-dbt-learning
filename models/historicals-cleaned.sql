43cc0613937cc72284988e0d286ceafe80991ac7
Jeff Shearer <blue.joy2746@fastmail.com>
2022-06-03 20:36:13 +0000

    updated with tutorial examples

--- models/historicals-cleaned.sql
+++
@@ -1,6 +0,0 @@
-SELECT *,
-{{dbt_utils.dateadd(datepart='day', interval='age', from_date_or_timestamp='created') }} as closed
-FROM `lofty-dynamics-283618.demand_model.historical_optys` as historicals 
-
-WHERE team is not null
-
